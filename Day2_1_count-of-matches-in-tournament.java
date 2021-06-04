b500bb18be94aa6ed9dac9416f5fe77703cfb11e
Arusara <42410629+Arusara@users.noreply.github.com>
2021-06-04 08:55:33 +0530

    Create Day2_1_count-of-matches-in-tournament.java

---
+++ Day2_1_count-of-matches-in-tournament.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String restoreString(String s, int[] indices) {
+      
+      StringBuilder str = new StringBuilder(s);
+      for (int i=0;i<s.length();i++) {
+        
+        str.setCharAt(indices[i], s.charAt(i));
+        
+      }
+      return str.toString();
+        
+    }
+}
