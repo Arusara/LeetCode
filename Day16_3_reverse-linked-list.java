1c3311b75b1d5aaf1c230c827a467330e6124903
Arusara <42410629+Arusara@users.noreply.github.com>
2021-06-19 18:55:46 +0530

    Create Day16_3_reverse-linked-list.java

---
+++ Day16_3_reverse-linked-list.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+      
+      ListNode prev = null;
+      ListNode temp = head;
+      ListNode curr = head;
+      
+      while(curr!=null) {
+        head = curr;
+      curr=curr.next;          
+      temp.next = prev;
+      prev = temp;
+      temp = curr;
+      }
+      
+      
+      return head;
+        
+    }
+}
