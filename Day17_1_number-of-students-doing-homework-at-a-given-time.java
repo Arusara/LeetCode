99569206c91d5fcb1d421ed0f2b86ca734586a50
Arusara <42410629+Arusara@users.noreply.github.com>
2021-06-20 12:20:39 +0530

    Create Day17_1_number-of-students-doing-homework-at-a-given-time.java

---
+++ Day17_1_number-of-students-doing-homework-at-a-given-time.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int busyStudent(int[] startTime, int[] endTime, int queryTime) {
+      
+      int n = startTime.length;
+      int counter = 0;
+      for (int i=0;i<n;i++) {
+        
+        if (queryTime>=startTime[i]&&queryTime<=endTime[i]) {
+          counter++;
+        }
+        
+      }
+      return counter;
+    }
+}
