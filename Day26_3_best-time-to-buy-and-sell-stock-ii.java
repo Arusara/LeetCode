da6fb6472898517b80b24ccc42b1527894a4c20e
Arusara <42410629+Arusara@users.noreply.github.com>
2021-07-02 16:52:23 +0530

    Create Day26_3_best-time-to-buy-and-sell-stock-ii.java

---
+++ Day26_3_best-time-to-buy-and-sell-stock-ii.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+    
+      int total = 0;
+      for (int i=1;i<prices.length;i++) {
+        
+        if (prices[i]>prices[i-1]) {
+          total = total + (prices[i] - prices[i-1]);
+        }
+        
+      }
+      return total;
+        
+    }
+}
