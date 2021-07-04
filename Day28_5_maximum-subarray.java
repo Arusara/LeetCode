b3238b37ac5aa0aa17cc8ebb2dcf96079df52dec
Arusara <42410629+Arusara@users.noreply.github.com>
2021-07-04 22:18:46 +0530

    Create Day28_5_maximum-subarray.java

---
+++ Day28_5_maximum-subarray.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+      
+      int max = Integer.MIN_VALUE;
+      for (int i=0;i<nums.length;i++) {
+        int sum=0;
+        for (int j=i;j<nums.length;j++) {
+          sum+=nums[j];
+          
+          if (sum>max) {
+            max = sum;
+          }
+        }
+        
+      }
+      return max;
+        
+    }
+}
