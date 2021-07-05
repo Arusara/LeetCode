b924b2797ae96350a6b5dc6ce0367a112274b9ca
Arusara <42410629+Arusara@users.noreply.github.com>
2021-07-05 15:51:09 +0530

    Create Day29_2_valid_palindrom_2.java

---
+++ Day29_2_valid_palindrom_2.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+      int i = 0;
+      int j = s.length()-1;
+      while(i<j) {
+        
+        if (s.charAt(i)!=s.charAt(j)) {
+          return isPalindrome(s, i+1, j) || isPalindrome(s, i, j-1);
+        }
+        
+        i++;
+        j--;
+        
+      }
+      return true;
+      
+        
+    }
+  
+  
+  public static boolean isPalindrome(String str, int i, int j) {
+    
+    while(i<j) {
+      
+      if (str.charAt(i)!=str.charAt(j)) return false;
+      i++;
+      j--;
+    }
+    return true;
+    
+    
+  }
+  
+}
