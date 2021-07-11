538503e5ff7ef86379a4c02bad6b6d7832c7673d
Arusara <42410629+Arusara@users.noreply.github.com>
2021-07-11 20:11:11 +0530

    Create Day31_1_third-maximum-number.java

---
+++ Day31_1_third-maximum-number.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int thirdMax(int[] nums) {
+      
+      long firstMax = Long.MIN_VALUE;
+      long secondMax = Long.MIN_VALUE;
+      long thirdMax = Long.MIN_VALUE;
+      
+      
+      for (int i=0;i<nums.length;i++) {
+        
+        if (nums[i]>firstMax) {
+          thirdMax = secondMax;
+          secondMax = firstMax;
+          firstMax = nums[i];
+        } else if (nums[i]>secondMax && nums[i]<firstMax) {
+          thirdMax = secondMax;
+          secondMax = nums[i];
+        } else if (nums[i]>thirdMax && nums[i]<secondMax) {
+          thirdMax = nums[i];
+        }
+        
+      }
+      
+      return thirdMax != Long.MIN_VALUE ?  (int)thirdMax :(int) firstMax;
+        
+    }
+}
