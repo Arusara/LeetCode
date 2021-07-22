d695a72641e9d443f328e9da4594589a7f056903
Arusara <42410629+Arusara@users.noreply.github.com>
2021-07-22 12:41:39 +0530

    Create Day39_1_container-with-most-water.java

---
+++ Day39_1_container-with-most-water.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxArea(int[] height) {
+      int i = 0;
+      int j = height.length-1;
+      
+      int max = Integer.MIN_VALUE;
+      
+      while(i<j) {
+        
+        int min = Math.min(height[i], height[j]);
+        max = Math.max(max, min*(j-i));
+        
+        if (height[i]<height[j]) {
+          i++;
+        } else {
+          j--;
+        }
+        
+      }
+      return max;
+      
+        
+    }
+}
