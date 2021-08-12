bb48092fbe10a0dce7097208f3657b2fb599a53b
Arusara <42410629+Arusara@users.noreply.github.com>
2021-08-12 09:16:23 +0530

    Create Day53_3_4_sum_II.java

---
+++ Day53_3_4_sum_II.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
+      
+      Map<Integer, Integer> map = new HashMap<>();
+      
+      for (int i=0;i<nums1.length;i++) {
+        for(int j=0;j<nums2.length;j++) {
+          map.put(nums1[i]+nums2[j], map.getOrDefault(nums1[i]+nums2[j], 0)+1);
+        }
+      }
+      int result=0;
+      for(int i=0;i<nums3.length;i++) {
+        
+        for(int j=0;j<nums4.length;j++) {
+          int temp = -nums3[i]-nums4[j];
+          
+          if (map.containsKey(temp)) {
+            result+=map.get(temp);
+          }
+        }
+        
+      }
+      
+      return result;
+        
+    }
+}
