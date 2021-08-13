f8c727aede9eeb0756006e7d35fff1d23adbe634
Arusara <42410629+Arusara@users.noreply.github.com>
2021-08-13 16:25:29 +0530

    Create Day54_1_maximum_gap.java

---
+++ Day54_1_maximum_gap.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maximumGap(int[] nums) {
+      
+      if (nums.length<2) return 0;
+      Arrays.sort(nums);
+      
+      int max = 0;
+      
+      for (int i=1;i<nums.length;i++) {
+
+        int diff = nums[i]-nums[i-1];
+        max = Math.max(max, diff);
+        
+      }
+      
+      return max;
+        
+    }
+}
