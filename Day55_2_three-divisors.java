955fe537d5f0bd53dec462764c8704434bf140bb
Arusara <42410629+Arusara@users.noreply.github.com>
2021-08-14 18:53:21 +0530

    Create Day55_2_three-divisors.java

---
+++ Day55_2_three-divisors.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isThree(int n) {
+      
+      int count=2;
+      
+      for(int i=2;i<=n/2;i++) {
+        
+        if (n%i==0) count++;
+        
+      }
+      
+      if (count==3) return true;
+      return false;
+        
+    }
+}
